66a2075d2eb94a17b755920ba17a182a801e709b
Arvib Fazriansyah <85182275+arv-fazriansyah@users.noreply.github.com>
2023-11-06 01:26:00 +0700

    Update unblock.bat

--- unblock.bat
+++ unblock.bat
@@ -1,34 +1,20 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
-set /p file_name=Masukkan nama file tanpa ekstensi: 
-
-:: Periksa apakah file tersebut ada dengan ekstensi .xlsb
-if exist "%file_name%.xlsb" (
-  set "excel_extension=xlsb"
-)
-
-:: Periksa apakah file tersebut ada dengan ekstensi .xlsm jika belum ditemukan dengan .xlsb
-if not defined excel_extension (
-  if exist "%file_name%.xlsm" (
-    set "excel_extension=xlsm"
-  )
+:: Mencari file dengan ekstensi .xlsb atau .xlsm
+for %%f in (*.xlsb *.xlsm) do (
+  set "excel_extension=%%~xf"
+  set "file_name=%%~nf"
+  powershell -command "Unblock-File -Path '!file_name!!excel_extension!'"
+  powershell -command "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Office\*\Excel\Security' -Name 'ProtectedView' -Value 0"
+  powershell -command "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Office\*\Excel\Security' -Name 'VBAWarnings' -Value 1"
+  echo File: "!file_name!!excel_extension!" diaktifkan.
 )
 
 :: Jika tidak ada file yang ditemukan, keluar
 if not defined excel_extension (
-  echo File tidak ditemukan: "%file_name%.xlsb" atau "%file_name%.xlsm"
+  echo Tidak ada file .xlsb atau .xlsm ditemukan.
   exit /b
 )
 
-:: Jalankan perintah PowerShell untuk mengaktifkan Unblock pada file
-powershell -command "Unblock-File -Path '%file_name%.%excel_extension%'"
-
-:: Jalankan perintah PowerShell untuk menonaktifkan Protected View
-powershell -command "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Office\*\Excel\Security' -Name 'ProtectedView' -Value 0"
-
-:: Jalankan perintah PowerShell untuk mengaktifkan Enable Content
-powershell -command "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Office\*\Excel\Security' -Name 'VBAWarnings' -Value 1"
-
-echo Opsi 'Unblock', 'Protected View', dan 'Enable Content' telah diaktifkan untuk file: "%file_name%.%excel_extension%"
 pause
