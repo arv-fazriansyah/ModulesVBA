cb2a364b8e366773f58ace235eafcf90268e2068
Arvib Fazriansyah <85182275+arv-fazriansyah@users.noreply.github.com>
2023-11-09 07:10:21 +0700

    Update GenerateToken.js

--- GenerateToken.js
+++ GenerateToken.js
@@ -1,40 +1,39 @@
-# APPSCRIPT
 function onOpen() {
   var ui = SpreadsheetApp.getUi();
-  ui.createMenu('Generate Token')
-      .addItem('Generate', 'generateToken')
+  ui.createMenu('Generate')
+      .addItem('Generate Tokens', 'generateTokens')
       .addToUi();
 }
 
-function generateToken() {
+function generateTokens() {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('DATA');
-  var dataRange = sheet.getRange('A2:A');
-  var tokenRange = sheet.getRange('F2:F');
-  
-  // Dapatkan data teks dari kolom A
-  var dataValues = dataRange.getValues();
-  
-  var tokens = dataValues.map(function(row) {
-    if (row[0] !== "") {
-      return [generateRandomToken()];
-    } else {
-      return [""];
-    }
-  });
+  var dataColumn = sheet.getRange('A:A').getValues();
+  var outputColumn = sheet.getRange('F:F');
 
-  // Tulis token ke kolom F
-  tokenRange.setValues(tokens);
+  // Hapus konten di kolom output
+  outputColumn.clearContent();
+
+  // Tambahkan header "GENERATE" ke kolom output
+  outputColumn.setValue([['GENERATE']]);
+
+  // Loop melalui data di kolom A dan hasilkan token
+  for (var i = 1; i < dataColumn.length; i++) {
+    var cellValue = dataColumn[i][0];
+    if (cellValue !== "") {
+      var token = generateRandomToken(5);
+      outputColumn.getCell(i + 1, 1).setValue(token);
+    } else {
+      outputColumn.getCell(i + 1, 1).setValue("");
+    }
+  }
 }
 
-function generateRandomToken() {
+function generateRandomToken(length) {
   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  var charactersLength = characters.length;
   var token = '';
-
-  for (var i = 0; i < 5; i++) {
-    var randomIndex = Math.floor(Math.random() * charactersLength);
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * characters.length);
     token += characters.charAt(randomIndex);
   }
-
   return token;
 }
