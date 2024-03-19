f6700ed0626babff58c5138307972e432135c2c4
Arvib Fazriansyah <85182275+arv-fazriansyah@users.noreply.github.com>
2024-03-20 01:32:43 +0700

    Update Proxy.js

--- Proxy.js
+++ Proxy.js
@@ -2,25 +2,22 @@ async function handleRequest(request, env) {
   const url = new URL(request.url);
   const path = url.pathname;
 
-  // Membuat pemetaan jalur ke URL target dari variabel lingkungan
   const pathToProxyUrl = {
-    [`/${env.TOKEN}`]: env.TOKEN_URL,
     [`/${env.DATA}`]: env.DATA_URL,
     [`/${env.FORMULA}`]: env.FORMULA_URL,
+    [`/${env.DEV}`]: env.DEV_URL,
+    [`/${env.SEND_DATA}`]: env.SEND_DATA_URL,
   };
 
-  // Memeriksa apakah jalur ada dalam pemetaan
-  if (path in pathToProxyUrl) {
-    const proxyUrl = pathToProxyUrl[path];
-    return await fetch(proxyUrl);
+  const proxyUrl = pathToProxyUrl[path];
+  if (proxyUrl) {
+    return await fetch(proxyUrl, request);
   }
 
-  // Mengembalikan respons 404 untuk jalur lainnya
   return new Response('Not Found', { status: 404 });
 }
 
 export default {
-  // Fungsi untuk menangani permintaan fetch
   async fetch(request, env, ctx) {
     return await handleRequest(request, env);
   },
